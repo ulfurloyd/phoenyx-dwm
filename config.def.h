9c93f17974b840cb9ec821f7270bc956a698958b
ulfurloyd <eklavyasood@gmail.com>
2025-10-16 01:40:03 +0530

    feat: bar height patch impl

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 10;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "Fira Code Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "Fira Code Nerd Font Mono:size=12";
 
