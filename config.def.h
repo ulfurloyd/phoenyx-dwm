399a6f203a49ea7c1f8a362f6ffb74354c2a73dc
ulfurloyd <eklavyasood@gmail.com>
2025-10-18 20:40:35 +0530

    feat: exitmenu patch implemented

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const int          topbar      = 1;        /* 0 means bottom bar */
 static const int          vertpad     = 10;       /* vertical padding of bar */
 static const int          sidepad     = 10;       /* horizontal padding of bar */
 static const int          user_bh     = 8;        /* 2 is the default spacing around the bar's font */
-static const char         *fonts[]    = { "Fira Code Nerd Font Mono:size=12" };
-static const char         dmenufont[] = "Fira Code Nerd Font Mono:size=12";
+static const char         *fonts[]    = { "FiraCode nerd Font Mono:size=12" };
+static const char         dmenufont[] = "FiraCode nerd Font Mono:size=12";
 static unsigned int baralpha        = 0x00;
 static unsigned int borderalpha     = OPAQUE;
 
