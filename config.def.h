323512b7379e8130d9cc0a266a217dbed69a0879
ulfurloyd <eklavyasood@gmail.com>
2025-10-19 12:37:37 +0530

    feat: font size and new block scripts

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const int          topbar      = 1;        /* 0 means bottom bar */
 static const int          vertpad     = 10;       /* vertical padding of bar */
 static const int          sidepad     = 10;       /* horizontal padding of bar */
 static const int          user_bh     = 8;        /* 2 is the default spacing around the bar's font */
-static const char         *fonts[]    = { "FiraCode nerd Font Mono:size=12" };
+static const char         *fonts[]    = { "FiraCode nerd Font Mono:size=14" };
 static const char         dmenufont[] = "FiraCode nerd Font Mono:size=12";
 static unsigned int baralpha        = 0x00;
 static unsigned int borderalpha     = OPAQUE;
--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,8 +18,11 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "~/Dev/dwl/someblocks/scripts/weather", 300, 0) \
+    X("", "blocks-musicplaying", 1, 0) \
     X("", "~/Dev/dwl/someblocks/scripts/volume", 1, 10) \
-    X("", "date '+%b %d (%a) %R:%S'", 1, 10)
+    X("", "blocks-systemstats", 30, 0) \
+    X("", "blocks-disks", 600, 0) \
+    X("", "~/Dev/dwl/someblocks/scripts/weather", 300, 0) \
+    X("", "blocks-timedate", 1, 10)
 
 #endif  // CONFIG_H
